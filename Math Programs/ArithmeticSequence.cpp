eaadf48c5b157160c38c26d79089ea1f152ad150
Vivaan Gupta <155184535+vivaan3141@users.noreply.github.com>
2025-12-28 09:13:25 +0530

    Update ArithmeticSequence.cpp

--- Math Programs/ArithmeticSequence.cpp
+++ Math Programs/ArithmeticSequence.cpp
@@ -3,20 +3,15 @@
 using namespace std;
 
 int main(){   
-    
-    int c[]={};
-    int a;
+    int c[]={}; int a;
     
     for (int i=0; i<3; i++){
         cout<<"Number"<<i+1<<":";
         cin>>a;
         c[i]=a;
     }
-    int n;
-    int two= c[1];
-    int three=c[2];
-    int d=three-two;
-    int com=c[0]-d;
+    
+    int n; int two= c[1]; int three=c[2]; int d=three-two; int com=c[0]-d;
     
     if (com>0){
         cout<<"The equation of this series is "<<d<<"n"<<"+"<<com<<endl;  
@@ -25,14 +20,14 @@ int main(){
     }else if (com==0){
         cout<<"The equation of this series is "<<d<<"n"<<endl;  
     }
-    int val=0;
-    int find=0;
-    cout<<"What number in the series would you like to find?"<<endl;
-    cin>>val;
+    
+    int val=0; int find=0;
+    
+    cout<<"What number in the series would you like to find?"<<endl;  cin>>val;
     find=(d*val);
+    
     cout<<"The "<<val<<" number in this series is "<<find+com;
       
-    return 0;
 }
 
 
