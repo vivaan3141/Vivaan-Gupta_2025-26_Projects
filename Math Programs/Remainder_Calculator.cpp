2582be39079325ac485b602f50e3556feab1c53c
Vivaan Gupta <155184535+vivaan3141@users.noreply.github.com>
2025-12-28 09:11:43 +0530

    Update Remainder_Calculator.cpp

--- Math Programs/Remainder_Calculator.cpp
+++ Math Programs/Remainder_Calculator.cpp
@@ -4,23 +4,14 @@
 
 using namespace std;
 int main() {
-    int div1;
-    int div2;
-    double quo;
-    cout<<"1:"<<endl;
-    cin>>div1;
-    cout<<"2:"<<endl;
-    cin>>div2;
+    int div1, div2; double quo;
+    cout<<"1:"<<endl; cin>>div1; cout<<"2:"<<endl; cin>>div2;
     quo=div1/div2;
     if (div1%div2==0){
         cout<<"Solution: "<<quo;
     }else{
-        quo=floor(quo);
-        cout<<"Solution: "<<quo<<" Remainder "<< (div1%div2);
-        
+        quo=floor(quo); cout<<"Solution: "<<quo<<" Remainder "<< (div1%div2);   
     }
-    
-
     return 0;
 }
 
